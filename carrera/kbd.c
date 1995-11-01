0cb1fb759a8864ab575c0a67ca4d6ba27b41d6c3
David du Colombier <0intro@gmail.com>
1995-11-01 00:00:00 +0000

    Plan 9 from Bell Labs 1995-11-01

--- carrera/kbd.c
+++ carrera/kbd.c
@@ -342,7 +342,7 @@ mousecmd(int cmd)
 		c = KBDDAT;
 	} while(c == 0xFE || c == 0);
 	if(c != 0xFA){
-		print("mouse returns %2.2ux to the %2.2ux command\n", c, cmd);
+		/*print("mouse returns %2.2ux to the %2.2ux command\n", c, cmd);/**/
 		return -1;
 	}
 	return 0;
