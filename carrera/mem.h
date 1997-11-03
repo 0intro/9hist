461feefecfe751634ada5190e6569d67c9ff9323
David du Colombier <0intro@gmail.com>
1997-11-03 00:00:00 +0000

    Plan 9 from Bell Labs 1997-11-03

--- carrera/mem.h
+++ carrera/mem.h
@@ -9,12 +9,12 @@
 #define	BI2BY		8			/* bits per byte */
 #define	BI2WD		32			/* bits per word */
 #define	BY2WD		4			/* bytes per word */
-#define BY2V		8			/* bytes per vlong */
+#define 	BY2V		8			/* bytes per vlong */
 #define	BY2PG		4096			/* bytes per page */
 #define	WD2PG		(BY2PG/BY2WD)		/* words per page */
 #define	PGSHIFT		12			/* log(BY2PG) */
-#define ROUND(s, sz)	(((s)+(sz-1))&~(sz-1))
-#define PGROUND(s)	ROUND(s, BY2PG)
+#define 	ROUND(s, sz)	(((s)+(sz-1))&~(sz-1))
+#define 	PGROUND(s)	ROUND(s, BY2PG)
 
 #define	MAXMACH		1			/* max # cpus system can run */
 #define	KSTACK		4096			/* Size of kernel stack */
