77f4134014e5b1c5c71ee79ca9aa446cecd089bd
David du Colombier <0intro@gmail.com>
1998-12-22 00:00:00 +0000

    Plan 9 from Bell Labs 1998-12-22

--- pc/archgeneric.c
+++ pc/archgeneric.c
@@ -92,8 +92,8 @@ static X86type x86amd[] =
 	{ 5,	3,	23,	"AMD-K5", },	/* guesswork */
 	{ 5,	6,	11,	"AMD-K6", },	/* determined by trial and error */
 	{ 5,	7,	11,	"AMD-K6", },	/* determined by trial and error */
-	{ 5,	8,	23,	"AMD-K6 3D", },	/* guesswork */
-	{ 5,	9,	23,	"AMD-K6 3D+", },/* guesswork */
+	{ 5,	8,	11,	"AMD-K6 3D", },	/* guesswork */
+	{ 5,	9,	11,	"AMD-K6 3D+", },/* guesswork */
 
 	{ 4,	-1,	22,	"Am486", },	/* guesswork */
 	{ 5,	-1,	23,	"AMD-K5/K6", },	/* guesswork */
