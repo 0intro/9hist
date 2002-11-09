2acb671fa0b58d3b8903d6e02737f7bbc5df0ca1
David du Colombier <0intro@gmail.com>
2002-11-09 00:00:00 +0000

    Plan 9 from Bell Labs 2002-11-09

--- port/devmouse.c
+++ port/devmouse.c
@@ -124,7 +124,7 @@ mousewalk(Chan *c, Chan *nc, char **name, int nname)
 	Walkqid *wq;
 
 	wq = devwalk(c, nc, name, nname, mousedir, nelem(mousedir), devgen);
-	if(wq != nil && wq->clone != c && (wq->clone->qid.type&QTDIR)==0)
+	if(wq != nil && wq->clone != c && wq->clone != nil && (wq->clone->qid.type&QTDIR)==0)
 		incref(&mouse);
 	return wq;
 }
